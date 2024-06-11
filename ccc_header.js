6e169a1ee2db0367cdf360fc7c8536fdb6b5f4b0
codenitrr <101342929+codenitrr@users.noreply.github.com>
2024-06-11 19:39:30 +0200

    Update ccc_header.js

--- ccc_header.js
+++ ccc_header.js
@@ -1,12 +1,30 @@
-let custom_auti = { "lang": "nl", "page": "cat" };
+let custom_auti = { "lang": "nl", "page": "home" };
 
 function log_custom() {
-    console.log(custom_auti.page)
+    console.log(custom_auti)
 }
 
 $(document).ready(function(log_custom) {
-    if ($('.ProductTemplate_Responsive').length > 0) {
+    if ($('.ProductTemplate_Responsive').length = 1) {
         custom_auti.page = "product";
     }
-});
-
+ ]  if   ($('.ShowProductCategoryHolder').length = 1) {
+            custom_auti.page = "category";
+     }
+}
+    
+$(document).ready(function() {
+    let textnl = "3 Willekeurige tangles met korting";
+    let textde = "3 Zufällige Verwicklungen mit Rabatt";
+    let textfr = "3 Des enchevêtrements aléatoires au rabais";
+    switch (custom_auti.lang) {
+        case "nl":
+            $('select[name="product_attribute[4882851]"] option[value="39487410"]').text(textnl);
+            break;
+        case "de":
+            $('select[name="product_attribute[4882851]"] option[value="39487410"]').text(textde);
+            break;
+        case "fr":
+            $('select[name="product_attribute[4882851]"] option[value="39487410"]').text(textfr);
+            break;
+    }
